26dc12a99fd6236ff5fdffc2a5b0942a50074748
Simon Orozco Henao <saimon2989@gmail.com>
2023-08-21 15:06:44 -0500

    adding styles to the registration form

--- src/app.js
+++ src/app.js
@@ -11,9 +11,9 @@ app.listen(PORT, ()=>{
 })
 
 app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, './views/home.html'))
+    res.sendFile(path.join(__dirname, '/views/home.html'))
 })
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/register.html'));
+    res.sendFile(path.join(__dirname, '/views/register.html'))
 });
