9ac47342d18772868f96597f18d6a6171d93c824
Simon Orozco Henao <saimon2989@gmail.com>
2023-08-21 15:29:55 -0500

    Adding the login.html file with your form and its styles.

--- src/app.js
+++ src/app.js
@@ -17,3 +17,7 @@ app.get('/', (req, res) =>{
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/register.html'))
 });
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+});
